f4a5d94f1dd5a74e8c7a068a4590b8723e752f24
SVarelaRamos <s.varelaramos@gmail.com>
2024-07-28 08:04:35 +0200

    initial commit

---
+++ src/data/wordpress.ts
@@ -0,0 +1,30 @@
+const WP_GRAPHQL_ENDPOINT = import.meta.env.WP_GRAPHQL_ENDPOINT;
+const WP_GRAPHQL_USERNAME = import.meta.env.WP_GRAPHQL_USERNAME;
+const WP_GRAPHQL_PASSWORD = import.meta.env.WP_GRAPHQL_PASSWORD;
+
+interface WPGraphQLParams {
+  query: string;
+  variables?: object;
+}
+
+console.log(WP_GRAPHQL_ENDPOINT);
+
+export async function wpquery({ query, variables = {} }: WPGraphQLParams) {
+  const res = await fetch(WP_GRAPHQL_ENDPOINT, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Basic " + btoa(WP_GRAPHQL_USERNAME + ":" + WP_GRAPHQL_PASSWORD),
+    },
+    body: JSON.stringify({ query, variables }),
+  });
+
+  if (!res.ok) {
+    console.error(res);
+    return {};
+  }
+
+  const { data } = await res.json();
+  return data;
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        chubbo: ["Chubbo-Bold", "serif"],
+        supreme: ["Supreme-Medium", "sans-serif"],
+      },
+      boxShadow: {
+        bold: "6px 6px 0 0 rgba(0, 0, 0, 1)",
+        "bold-sm": "4px 4px 0 0 rgba(0, 0, 0, 1)",
+        "bold-lg": "8px 8px 0 0 rgba(0, 0, 0, 1)",
+        "bold-xl": "12px 12px 0 0 rgba(0, 0, 0, 1)",
+      },
+    },
+  },
+  plugins: [],
+};
